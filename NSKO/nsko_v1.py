cade6f57620a9d592c1fdc5a437dfba727a7babb
owl <owl@owlmail.com>
2018-10-13 13:31:57 +0300

    now V.transpose

--- NSKO/nsko_v1.py
+++ NSKO/nsko_v1.py
@@ -1,9 +1,25 @@
+
+
 import numpy as np
 
 # default_intro
 def additional_constructions(ar_x, ar_cl):
-    array_of_Y = np.ones((len(ar_x), 1))
-    print(array_of_Y)
+    array_of_y = np.ones((1, len(ar_x)))
+    # print(array_of_y[0][2])
+
+    # ar_x_with_class = []
+    for i in range(len(ar_x)):
+        ar_x[i].append(1 if ar_cl[i] == 0 else -1)
+    print(ar_x)
+
+    for i in range(len(ar_x)):
+        if i == 0:
+            matrix_v = np.array(ar_x[i])
+        else:
+            matrix_v = np.vstack((matrix_v, ar_x[i]))
+    matrix_v = matrix_v.transpose()
+    print(matrix_v)
+
     pass
 
 if __name__ == '__main__':
