bb099bde9a96b08e25c3fe577d6341303b0a6850
owl <owl@owlmail.com>
2018-12-08 11:53:26 +0300

    add loader and gitignore

---
+++ NSKO/load_from_file.py
@@ -0,0 +1,16 @@
+def load_for_nsko(path):
+    with open(path, 'r') as file:
+        lines = file.readlines()
+
+        lines = [line.strip() for line in lines]
+        lines = [[symbol for symbol in line.split(' ')] for line in lines]
+
+        array_classes = [line.pop(-1) for line in lines]
+
+        # print(lines, '\n', array_classes)
+        return lines, array_classes
+
+
+if __name__ == '__main__':
+    print('\n\n\tTEST WORK MODE!', end='\n\n\n')
+    load_for_nsko('input_file.txt')
